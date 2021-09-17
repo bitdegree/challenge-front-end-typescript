f894b056e303e06b94ebb962df50d8f53386c91f
ibrahim.ozdemir <ibrahim.ozdemir@outlook.com.tr>
2021-09-17 21:53:58 +0300

    down arrow smoother

--- blog/src/app/pages/home/components/down-arrow-bouncer/down-arrow-bouncer.component.ts
+++ blog/src/app/pages/home/components/down-arrow-bouncer/down-arrow-bouncer.component.ts
@@ -1,18 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-down-arrow-bouncer',
   templateUrl: './down-arrow-bouncer.component.html',
-  styleUrls: ['./down-arrow-bouncer.component.css'],
-  animations:[    
-    trigger('move', [
-      transition(':enter', [
-        style({height:0}),
-        animate('300ms', style({ height:500}))
-      ]),
-    ]),
-    ]
+  styleUrls: ['./down-arrow-bouncer.component.css']
 })
 export class DownArrowBouncerComponent implements OnInit {
 
@@ -21,20 +12,8 @@ export class DownArrowBouncerComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  state = 'in';
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.state = 'out';
-    }, 0);
-  }
-
-  onEnd(event : any) {
-    this.state = 'in';
-    if (event.toState === 'in') {
-      setTimeout(() => {
-        this.state = 'out';
-      }, 0);
-    }
+  toBottom(){
+    window.scrollTo(0,document.body.scrollHeight);
   }
 
 }
