d24b00f17c563137cf5c7e95bb7d41a3783e0543
ibrahim.ozdemir <ibrahim.ozdemir@outlook.com.tr>
2021-09-17 14:42:20 +0300

    post service added

---
+++ blog/src/app/services/post.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { Observable } from "rxjs"
+import { environment } from "src/environments/environment"
+import { Post } from "../models/post.model"
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class PostService {
+
+    constructor(readonly httpClient : HttpClient){}
+
+    getPosts() : Observable<Post[]>{
+        return this.httpClient.get<Post[]>(environment.Endpoint.concat('posts')).pipe();
+    }
+
+}
